235c836516808234654656ad5f4fd08c579034b6
Keyston <keyston@conceptual-ideas.com>
2012-08-02 12:35:03 -0500

    -update display player

--- public/javascripts/app/controller/display.js
+++ public/javascripts/app/controller/display.js
@@ -23,14 +23,19 @@ define(["underscore", "backbone", "jwplayer", "app/common"], function (_, Backbo
 
     })
 
+    var State = {
+        BUSY:"busy",
+        PLAYING:"playing",
+        PLAY:"play"
+    }
     var Player = Backbone.View.extend({
         el:"#player",
         events:{
             "click a.play":"play",
-            "click a.pause":"pause",
+            "click a.playing":"pause",
             "click a.next":"next",
-            "click a.prev":"prev",
-            "click .thumb":"startDrag"
+            "click a.prev":"prev"
+
         },
         initialize:function (options) {
             this.player = jwplayer('player-proxy-inner');
@@ -42,68 +47,129 @@ define(["underscore", "backbone", "jwplayer", "app/common"], function (_, Backbo
                 'width':'470',
                 'height':'24'
             });
+            this.items = options.files;
             this.player
                 .onTime(_.bind(this._onTime, this))
                 .onPause(_.bind(this._onPause, this))
                 .onReady(_.bind(this._onReady, this))
+                .onPlay(_.bind(this._onPlay, this))
+                .onMeta(_.bind(this._onMeta, this))
+                .onBufferChange(_.bind(this._onBufferChange, this))
 
 
             this.$action = this.$(".action");
-            this.$slider = this.$(".slider").slider({
+            this.$slider = this.$(".slider .bar").slider({
                 slide:_.bind(this._onSliding, this),
-                stop:_.bind(this._onSlideStop, this)
+                stop:_.bind(this._onSlideStop, this),
+                start:_.bind(this._onSlideStart, this)
             });
-            this.s = this.$slider.slider;
+            this._currentIndex = 0;
+            this.$title = this.$(".title");
+            this.$time = this.$(".time");
+            this.$fill = this.$(".fill");
+            this.$next = this.$(".next");
+            this.$prev = this.$(".prev")
+
+
         },
         _onReady:function () {
-            this.debug(this.options.files);
-            this.player.load(this.options.files);
+
+            this.player.load(this.items);
         },
         _onSliding:function (event, ui) {
-
+            this.render(ui.value);
         },
         _onSlideStop:function (event, ui) {
-
+            this.seek(ui.value);
+            this.play();
+        },
+        _onSlideStart:function (e) {
+            this.pause();
+        },
+        _state:function (state) {
+            this.$action.removeClass().addClass("action " + state);
+            this.currentState = state;
         },
         _onPause:function (event) {
-            this.$action.removeClass("").addClass("action play");
+            this._state(State.PLAY)
         },
+        _onMeta:function () {
 
+        },
         _onPlay:function (event) {
-            this.$action.removeClass("").addClass("action pause");
 
+            if (event.oldstate == "PAUSED" || event.oldstate == "BUFFERING") {
+                //   this._state(State.PLAYING)
+            }
+
+        },
+        _onBufferChange:function (event) {
+            this.$fill.animate({width:event.bufferPercent + "%"});
         },
         _onTime:function (event) {
-            this.s("value", event.position);
+            if (this.currentState == State.BUSY && event.position > 1) {
+
+                this._state(State.PLAYING);
+                this.s("option", {
+                    min:0,
+                    max:event.duration
+                })
+
+            }
+            this.$slider.slider("value", event.position);
+            this.render();
         },
         item:function (index) {
             this.player.playlistItem(index);
+            this._currentIndex = index;
+            this._state(State.BUSY);
+            this.render();
         },
         play:function () {
-            this.player.play();
+            this._state(State.BUSY);
+            this.player.play(true);
         },
         pause:function () {
-            this.player.pause();
+            this.player.pause(true);
         },
         next:function () {
             this.player.playlistNext();
+            this.render();
+
         },
         prev:function () {
             this.player.playlistPrev()
-        },
-        seek:function () {
+            this.render();
 
         },
-        startDrag:function (e) {
 
+        seek:function (position) {
+            this.player.seek(position)
         },
+
         _format:function (seconds) {
-            var mins = Math.floor(seconds / 60)
+            var m = Math.floor(seconds / 60)
+            var s = Math.floor(seconds % 60);
+
+            return this.zero(m) + ":" + this.zero(s);
+        },
+        zero:function (nbr) {
+            if (nbr < 10) {
+                return '0' + nbr;
+            } else {
+                return '' + nbr;
+            }
         },
         render:function (position) {
-            position = position || this.s("value");
+            position = position || this.$slider.slider("value");
+            var item = this.items[this._currentIndex];
+            this.$title.text(item.title)
+            this.$time.text(this._format(position) + "/" + this._format(item.duration));
 
-            this.$time.text()
+            var length = this.items.length;
+            this.$next[length > 1 && this._currentIndex <= length - 1 ? "show" : "hide"]();
+
+            this.$prev[this._currentIndex > 0 ? "show" : "hide"]();
         }
 
     })
